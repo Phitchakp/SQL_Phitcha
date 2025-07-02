e52a12d5d4acef45bf47b1711a849d07a5a730ae
Phitcha Khrueapaeng <phitchakp@gmail.com>
2025-07-03 00:22:58 +0200

    start microsoft

--- Microsoft_Teams Power Users_(count, limit).sql
+++ Microsoft_Teams Power Users_(count, limit).sql
@@ -1,4 +1,4 @@
 -- in August 2022. Display the IDs of these 2 users along with the total number of messages they sent.
 
-
+-- ## COUNT >> how many rows messages in group by ID, return all values (duplicated).
 
